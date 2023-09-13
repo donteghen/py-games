2d6446da5e1b6eb9194ca685539ebc346044a8a1
donteghen <donaldteghen@gmail.com>
2023-09-13 05:30:29 +0100

    Completed the bitmapmessage.

---
+++ bitmapmessage.py
@@ -0,0 +1,28 @@
+import  sys
+
+message = input('Enter name for bitmap > ')
+spaces = input('Enter a special for spaces. Here are your options: (s : space | d for dash | + for plus) >')
+empty = '💔'
+if message == ' ' or len(message) == 0: 
+    sys.exit()
+if (spaces == 's'): 
+    empty = ' ' 
+elif spaces == 'd':
+    empty = '-' 
+elif spaces == '+':
+    empty = '+'
+bitmap = open('bitmap.txt', mode='r', encoding='utf-8').readlines()
+_bitmap = []
+for line in bitmap:
+    _line = ''
+    for i, bit in enumerate(line):
+        if bit == ' ':
+            _line += empty
+        else:
+            _line += message[i % len(message)]
+    _line += '\n'        
+    _bitmap.append(_line)        
+              
+f = open('bitmapresult.txt', mode='w', encoding='utf-8')
+f.writelines(_bitmap)
+f.close()
